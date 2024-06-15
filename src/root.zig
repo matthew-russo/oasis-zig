a12b3a80665ea490a477ed51e6981a342fd820eb
Matthew Russo <matthew@matthewclayrusso.com>
2024-06-15 12:47:48 -0700

    Root : adds utilities for stubbing out unfinished code

--- src/root.zig
+++ src/root.zig
@@ -2,6 +2,16 @@ const std = @import("std");
 
 pub const byte_buffer = @import("byte_buffer.zig");
 
+/// a panicking function for stubbing out general todos
+pub fn todo(msg: []const u8) noreturn {
+    std.debug.panic("[TODO] not yet implemented: {s}", .{msg});
+}
+
+/// a panicking function for stubbing out error handling
+pub fn errorHandlingPlaceholder(msg: []const u8) noreturn {
+    std.debug.panic("[TODO] handle error: {s}", .{msg});
+}
+
 test {
     std.testing.refAllDecls(@This());
 }
