5adc9b6c8e5c9872f554729cf41ebdbdf3910a52
Matthew Russo <matthew@matthewclayrusso.com>
2024-08-17 12:19:02 -0700

    ProjectStructure : starts giving some directory structure

---
+++ src/collections/mod.zig
@@ -0,0 +1,2 @@
+pub const byte_buffer = @import("byte_buffer.zig");
+pub const ring_buffer = @import("ring_buffer.zig");
--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
-pub const byte_buffer = @import("byte_buffer.zig");
+pub const collections = @import("collections/mod.zig");
 pub const debug_formatter = @import("debug_formatter.zig");
-pub const ring_buffer = @import("ring_buffer.zig");
 
 /// a panicking function for stubbing out general todos
 pub fn todo(msg: []const u8) noreturn {
